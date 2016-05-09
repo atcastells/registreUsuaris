238a837e8bb1de157096d32fa9de35af6cb3df4e
atcastells <aron.deltebre@gmail.com>
2016-05-09 12:02:31 +0200

    Codi Menu

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,5 +1,93 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Created by acastells on 02/05/16.
  */
 public class Biblioteca {
+    /*DECORACIÓ MENUS*/
+    public static void decoracio_menus(int longitud_array, String[] array){
+        String[] temp = array;
+        Scanner sc = new Scanner(System.in);
+        final String DECO = "·";//decoració que s'utiltizara
+        int longitud;//longitud de la línia mes llerga de la decoracio
+        //int contador=0;
+        String linia_decoracio =DECO;//línia superior i inferior de decoració
+
+        for (int i = 1; i < longitud_array; i++) {//afegim la decoració de la esquerra
+            array[i] = DECO + " " + array[i];
+        }
+
+        /*Contador de caracters*/
+        longitud = array[1].length();
+        for (int i = 1; i < longitud_array; i++) {//cerquem la línia amb més longitud
+            if (longitud < array[i].length()) {
+                longitud = array[i].length();
+            } else;
+        }
+
+        /*Decoració del menu (dreta)*/
+        for (int i = 1; i < longitud_array; i++) {//afegim espais a les línies per igualar-les
+            while (array[i].length() < longitud) {
+                array[i] =array[i] + " ";
+            }
+
+        }
+
+        for (int i = 1; i < longitud_array; i++) {//afegim un ultim espai i la decoració de la dreta
+            array[i] = array[i] + " " + DECO;
+        }
+
+
+        /*Decoració capsalera del menú*/
+        while(array[0].length()<longitud){ //centrem la capsalera segons la longitud
+            array[0]=" "+array[0]+" ";
+        }
+        array[0]=DECO+array[0]+DECO;
+        if(array[0].length()>array[1].length()){  //comprovem que els límits de la capsalera i les altres línies siguen igual
+            for(int i=1; i< longitud_array; i++){ //si la capsalera es més llerga, eliminem l'ultim caracter i afegim un espai i la decoracio de la dreta
+                array[i]= array[i].substring(0,array[i].length()-1);
+                array[i]= array[i]+" "+DECO;
+            }
+        }
+        /*lines inferior i superior*/
+        while (linia_decoracio.length() < array[0].length()) { //es creen les docarcións superiors i inferiors am la matèixa longitud que la resta de línies
+            linia_decoracio = linia_decoracio + DECO;
+        }
+
+        /*Imprimir menú*/
+        System.out.println(linia_decoracio);
+        for (int i = 0; i < longitud_array; i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println(linia_decoracio);
+
+
+    }
+    void llegirLinia(File arxiu){
+        try {
+            Scanner sc = new Scanner(arxiu);
+        }
+        catch (FileNotFoundException e){
+            imprimir("No hi ha arxiu\n");
+        }
+    }
+    void imprimir(String text){
+        PrintStream escriptor = new PrintStream(System.out);
+        escriptor.print(text);
+    }
+
+    int readInt(String missatge) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            imprimir(missatge);
+            return sc.nextInt();
+        } catch (InputMismatchException e) {
+            imprimir("Error d'entrada, introdueix una xifra!\n");
+            return (readInt(missatge));
+        }
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
         Principal programa = new Principal();
@@ -5,10 +9,80 @@ public class Principal {
     }
 
     void inici(){
-        /*Creem el fitxer*/
+        /*Instanciem Objectes*/
+        Biblioteca gui = new Biblioteca();
+        /*Definim el fitxer*/
+        File agenda = new File("Agenda.txt");
         /*Comprobem si el fitxer existeix*/
+        /*Comprobem si el fitxer te dades*/
+
         /*Si hi han dades les carreguem*/
         /*Menu*/
-        /*Escriptura al fitxer*/
+        int opcio= 0;//opció que escollira l'usuari
+        int opcio_submenu= 0;
+        int opcio_submenu_llistat=0;
+
+        while (opcio<menu_principal.length-1){
+            gui.decoracio_menus(menu_principal.length, menu_principal);
+            opcio = gui.readInt("Trieu una opcio: ");
+            switch(opcio){
+                case 1: //Registrar usuari
+                    break;
+                case 2: //Consultar usuari
+                    break;
+                case 3: //Menu eliminar
+                    while(opcio_submenu<menu_eliminar.length-1){
+                        gui.decoracio_menus(menu_eliminar.length, menu_eliminar);
+                        opcio_submenu = gui.readInt("Trieu una opcio: ");
+                        switch(opcio_submenu){
+                            case 1:
+                                break;
+                            case 2:
+                                break;
+                        }
+                    }
+                    break;
+                case 4: //Menu llistar
+                    while(opcio_submenu_llistat<menu_llistat.length-1){
+                        gui.decoracio_menus(menu_llistat.length, menu_llistat);
+                        opcio_submenu_llistat = gui.readInt("Trieu una opcio: ");
+                        switch(opcio_submenu_llistat){
+                            case 1:
+                                break;
+                            case 2:
+                                break;
+                        }
+                    }
+            }
+        }
     }
+
+        /*Escriptura al fitxer*/
+
+    /*FUNCIONS AUX PRINCIPAL*/
+    boolean comprovarFitxer (File arxiu){
+            if(arxiu.exists()){
+                return true;
+            }
+            else {
+                return false;
+            }
+    }
+
+    //Arrays Menus
+    //arrays de menus: menu_principal, menu_eliminar, menu_llistat
+    String[] menu_principal = {
+            "AGENDA",
+            "1) Registrar nou usuari",
+            "2) Consultar usuari",
+            "3) Esborrar usuari",
+            "4) Llistar usuaris",
+            "5) Sortir"
+    };
+    String[] menu_eliminar = {
+            "MENÚ ELIMINAR","1) Donar de baixa","2) Eliminar definitivament","3) Sortir"
+    };
+    String[] menu_llistat = {
+            "MENÚ LLISTAT","1) Usuaris d'alta","2) Usuaris de baix","3) Sortir"
+    };
 }
